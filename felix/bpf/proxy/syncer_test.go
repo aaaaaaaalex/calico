ba55cbda27f3611bae8089342e7750b7757e3d24
Alex O'Regan <alex.oregan@tigera.io>
2025-06-05 13:45:49 +0100

    write services to map in syncer/proxy

--- felix/bpf/proxy/syncer_test.go
+++ felix/bpf/proxy/syncer_test.go
@@ -47,6 +47,7 @@ var _ = Describe("BPF Syncer", func() {
 	var (
 		svcs      *mockNATMap
 		eps       *mockNATBackendMap
+		maglevEps *mockMaglevBackendMap
 		aff       *mockAffinityMap
 		ct        *mock.Map
 		ctCleanup *mock.Map
@@ -69,13 +70,14 @@ var _ = Describe("BPF Syncer", func() {
 	BeforeEach(func() {
 		svcs = newMockNATMap()
 		eps = newMockNATBackendMap()
+		maglevEps = newMockMaglevBackendMap()
 		aff = newMockAffinityMap()
 		ct = mock.NewMockMap(conntrack.MapParams)
 		ctCleanup = mock.NewMockMap(conntrack.MapParamsCleanup)
 
 		rt = proxy.NewRTCache()
 
-		s, _ = proxy.NewSyncer(4, nodeIPs, svcs, eps, aff, rt, nil)
+		s, _ = proxy.NewSyncer(4, nodeIPs, svcs, eps, maglevEps, aff, rt, nil)
 
 		ep := proxy.NewEndpointInfo("10.1.0.1", 5555, proxy.EndpointInfoOptIsReady(true))
 		state = proxy.DPSyncerState{
@@ -499,7 +501,7 @@ var _ = Describe("BPF Syncer", func() {
 		}))
 
 		By("resyncing after creating a new syncer with the same result", makestep(func() {
-			s, _ = proxy.NewSyncer(4, nodeIPs, svcs, eps, aff, rt, nil)
+			s, _ = proxy.NewSyncer(4, nodeIPs, svcs, eps, maglevEps, aff, rt, nil)
 			checkAfterResync()
 		}))
 
@@ -507,7 +509,7 @@ var _ = Describe("BPF Syncer", func() {
 			svcs.m[nat.NewNATKey(net.IPv4(5, 5, 5, 5), 1111, 6)] = nat.NewNATValue(0xdeadbeef, 2, 2, 0)
 			eps.m[nat.NewNATBackendKey(0xdeadbeef, 0)] = nat.NewNATBackendValue(net.IPv4(6, 6, 6, 6), 666)
 			eps.m[nat.NewNATBackendKey(0xdeadbeef, 1)] = nat.NewNATBackendValue(net.IPv4(7, 7, 7, 7), 777)
-			s, _ = proxy.NewSyncer(4, nodeIPs, svcs, eps, aff, rt, nil)
+			s, _ = proxy.NewSyncer(4, nodeIPs, svcs, eps, maglevEps, aff, rt, nil)
 			checkAfterResync()
 		}))
 
@@ -655,7 +657,7 @@ var _ = Describe("BPF Syncer", func() {
 
 		By("inserting non-local eps for a NodePort - no route", makestep(func() {
 			// use the meta node IP for nodeports as well
-			s, _ = proxy.NewSyncer(4, append(nodeIPs, net.IPv4(255, 255, 255, 255)), svcs, eps, aff, rt, nil)
+			s, _ = proxy.NewSyncer(4, append(nodeIPs, net.IPv4(255, 255, 255, 255)), svcs, eps, maglevEps, aff, rt, nil)
 			state.SvcMap[svcKey2] = proxy.NewK8sServicePort(
 				net.IPv4(10, 0, 0, 2),
 				2222,
@@ -808,7 +810,7 @@ var _ = Describe("BPF Syncer", func() {
 
 		By("inserting only non-local eps for a NodePort - multiple nodes & pods/node", makestep(func() {
 			// use the meta node IP for nodeports as well
-			s, _ = proxy.NewSyncer(4, append(nodeIPs, net.IPv4(255, 255, 255, 255)), svcs, eps, aff, rt, nil)
+			s, _ = proxy.NewSyncer(4, append(nodeIPs, net.IPv4(255, 255, 255, 255)), svcs, eps, maglevEps, aff, rt, nil)
 			state.SvcMap[svcKey2] = proxy.NewK8sServicePort(
 				net.IPv4(10, 0, 0, 2),
 				2222,
@@ -888,7 +890,7 @@ var _ = Describe("BPF Syncer", func() {
 
 		By("restarting Syncer to check if NodePortRemotes are picked up correctly", makestep(func() {
 			// use the meta node IP for nodeports as well
-			s, _ = proxy.NewSyncer(4, append(nodeIPs, net.IPv4(255, 255, 255, 255)), svcs, eps, aff, rt, nil)
+			s, _ = proxy.NewSyncer(4, append(nodeIPs, net.IPv4(255, 255, 255, 255)), svcs, eps, maglevEps, aff, rt, nil)
 			err := s.Apply(state)
 			Expect(err).NotTo(HaveOccurred())
 
@@ -1512,6 +1514,99 @@ func (m *mockNATBackendMap) Delete(k []byte) error {
 	}
 
 	var key nat.BackendKey
+	copy(key[:ks], k[:ks])
+
+	delete(m.m, key)
+
+	return nil
+}
+
+type mockMaglevBackendMap struct {
+	mock.DummyMap
+	sync.Mutex
+	m map[nat.MaglevBackendKey]nat.BackendValue
+}
+
+func (m *mockMaglevBackendMap) MapFD() maps.FD {
+	panic("implement me")
+}
+
+func newMockMaglevBackendMap() *mockMaglevBackendMap {
+	return &mockMaglevBackendMap{
+		m: make(map[nat.MaglevBackendKey]nat.BackendValue),
+	}
+}
+
+func (m *mockMaglevBackendMap) GetName() string {
+	return "maglevbe"
+}
+
+func (m *mockMaglevBackendMap) Path() string {
+	return "/sys/fs/bpf/tc/maglevbe"
+}
+
+func (m *mockMaglevBackendMap) Iter(iter maps.IterCallback) error {
+	m.Lock()
+	defer m.Unlock()
+
+	ks := len(nat.MaglevBackendKey{})
+	vs := len(nat.BackendValue{})
+	for k, v := range m.m {
+		action := iter(k[:ks], v[:vs])
+		if action == maps.IterDelete {
+			delete(m.m, k)
+		}
+	}
+
+	return nil
+}
+
+func (m *mockMaglevBackendMap) Update(k, v []byte) error {
+	logrus.WithFields(logrus.Fields{
+		"k": k, "v": v,
+	}).Debug("mockMaglevBackendMap.Update()")
+
+	m.Lock()
+	defer m.Unlock()
+
+	ks := len(nat.MaglevBackendKey{})
+	if len(k) != ks {
+		return fmt.Errorf("expected key size %d got %d", ks, len(k))
+	}
+	vs := len(nat.BackendValue{})
+	if len(v) != vs {
+		return fmt.Errorf("expected value size %d got %d", vs, len(v))
+	}
+
+	var key nat.MaglevBackendKey
+	copy(key[:ks], k[:ks])
+
+	var val nat.BackendValue
+	copy(val[:vs], v[:vs])
+
+	m.m[key] = val
+
+	return nil
+}
+
+func (m *mockMaglevBackendMap) Get(k []byte) ([]byte, error) {
+	panic("not implemented")
+}
+
+func (m *mockMaglevBackendMap) Delete(k []byte) error {
+	logrus.WithFields(logrus.Fields{
+		"k": k,
+	}).Debug("mockMaglevBackendMap.Delete()")
+
+	m.Lock()
+	defer m.Unlock()
+
+	ks := len(nat.MaglevBackendKey{})
+	if len(k) != ks {
+		return fmt.Errorf("expected key size %d got %d", ks, len(k))
+	}
+
+	var key nat.MaglevBackendKey
 	copy(key[:ks], k[:ks])
 
 	delete(m.m, key)
